b53437443dfbb3be572d8786d2645642c3ab59bc
Hariharasudhan177 <hari17792@gmail.com>
2024-10-16 23:18:32 +0100

    Add base landing page

--- UtavuCustomerFrontend/src/app/app.component.ts
+++ UtavuCustomerFrontend/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeroComponent } from './landing/hero/hero.component';
+import { FeaturesComponent } from './landing/features/features.component';
+import { HowitworksComponent } from './landing/howitworks/howitworks.component';
+import { ContactComponent } from './landing/contact/contact.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, 
+    HeaderComponent,
+    FooterComponent,
+    HeroComponent,
+    FeaturesComponent,
+    HowitworksComponent,
+    ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
