7de85868d3d186f19325e6e3e92df0d8c4922fd0
Hariharasudhan177 <hari17792@gmail.com>
2024-10-18 18:24:00 +0100

    Add gapi to index.html

--- UtavuCustomerFrontend/src/app/app.routes.ts
+++ UtavuCustomerFrontend/src/app/app.routes.ts
@@ -2,6 +2,5 @@ import { Routes } from '@angular/router';
 import { SignupComponent } from './account/signup/signup.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/', pathMatch: 'full' },
     { path: 'signup', component: SignupComponent },
 ];
