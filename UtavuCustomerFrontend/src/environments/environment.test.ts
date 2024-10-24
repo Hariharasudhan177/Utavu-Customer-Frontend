d6480b7ff8f25dfd6eff5adc57cac152e70b1e8c
Hariharasudhan177 <hari17792@gmail.com>
2024-10-24 13:56:06 +0100

    Add test environment

---
+++ UtavuCustomerFrontend/src/environments/environment.test.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://utavuconauth.kinddesert-b81ba258.australiaeast.azurecontainerapps.io',  // Production API URL
+    googleClientId: '602394270286-1prasg59qk03gaai95c4nllus0ooa0re.apps.googleusercontent.com' // Optional
+};
