a0419cc7be55cd04f099d792d138604c1c6fe657
Hariharasudhan177 <hari17792@gmail.com>
2024-10-24 22:30:40 +0100

    Add test environment variables

--- UtavuCustomerFrontend/src/environments/environment.test.ts
+++ UtavuCustomerFrontend/src/environments/environment.test.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://utavuconauth.kinddesert-b81ba258.australiaeast.azurecontainerapps.io',  // Production API URL
-    googleClientId: '602394270286-1prasg59qk03gaai95c4nllus0ooa0re.apps.googleusercontent.com' // Optional
+    apiUrl: 'https://utavuconauthtest.kinddesert-b81ba258.australiaeast.azurecontainerapps.io',  // Production API URL
+    googleClientId: '602394270286-3877qejc0nhqplh3hpeeafmj7419upc8.apps.googleusercontent.com' // Optional
 };
